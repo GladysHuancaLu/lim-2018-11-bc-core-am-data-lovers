3acf8ad390c1bad67c83d3abe91163e79694bc4c
GladysHuancaLu <gladys.luana123@gmail.com>
2018-12-14 15:25:53 -0500

    Subiendo cambios de funcion filtrar segun tipo y mostrando img

--- src/data.js
+++ src/data.js
@@ -1,8 +1,65 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+*/
+
+/*
+const obtenerTipoUnico = (arr) => {
+  var newArr = [];
+  var typesUniques = [];
+   for(var i = 0; i < arr.length; i++){
+      newArr.push(arr[i].type)
+      }
+   for(var j = 0; j < newArr.length; j++){
+     if(typesUniques.indexOf(newArr[j].join('')) === -1){
+       typesUniques.push(newArr[j].join(''))
+     }
+    }
+  return typesUniques;
+}
+
+window.pokemon = {
+
+ obtenerTipoUnico
+
+};
+
+*/
+const obtenerTipoUnico = (arr) => {
+  var newArr = [];
+  var typesUniques = [];
+   for(let i = 0; i < arr.length; i++){
+     newArr= newArr.concat(arr[i].type);
+   }
+   for(var j = 0; j < newArr.length; j++){
+    if(typesUniques.indexOf(newArr[j]) === -1){
+      typesUniques.push(newArr[j]);
+    }
+   }
+
+  return typesUniques;
+}
+
+
+
+
+
+//console.log(newArr2);
+console.log(POKEMON.pokemon.length)
+
+
+console.log(obtenerTipoUnico(POKEMON.pokemon));
+
+
+window.pokemon = {
+
+ obtenerTipoUnico
+
+
+};
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 let number = "aprendiendo";
 console.log(number);
-console.log("hola mundo");
 console.log(POKEMON);
 console.log(POKEMON.pokemon[1].name);
 
@@ -18,21 +17,54 @@ console.log(newArr);
 
 /*
 const newArr2 = POKEMON.pokemon.filter((pokemon) => {
-  return (pokemon.type[0] === 'Grass');
+  return ((pokemon.type[0] === "Poison" )||(pokemon.type[1] === "Poison")||(pokemon.type[2] === "Poison"));
 })
 */
 
 
 const filtrarTipo= (tipo) =>{
 
-  const newArr2=[];
-  newArr2.push(POKEMON.pokemon.filter((pokemon) => {
+ 
+  const newArr2= POKEMON.pokemon.filter((pokemon) => {
    return ((pokemon.type[0] === tipo )||(pokemon.type[1] === tipo)||(pokemon.type[2] === tipo));
- }))
- console.log(newArr2);
+ })
+ return newArr2;
 }
 
-filtrarTipo('Grass');
 
-//console.log(newArr2);
-console.log(POKEMON.pokemon.length)
+
+
+
+//console.table(POKEMON);
+const dataPokemon = POKEMON.pokemon;
+const arrayTiposUnicos =  pokemon.obtenerTipoUnico(dataPokemon);
+const listaTipos = document.getElementById('tipo');
+for(let i = 0; i < arrayTiposUnicos.length; i++){
+  listaTipos.innerHTML += `<option >${arrayTiposUnicos[i]}</option>`
+
+}
+
+
+function mostrar (tipo){
+  const containerlist = document.getElementById("pokemon");
+  let list  = '';
+  filtrarTipo(tipo).forEach((pokemones)  =>  {
+    const li  = `<li>${pokemones.name}</li>`  + `<img src="${pokemones.img}">`;
+    list  = list  + li;
+    
+  })
+  containerlist.innerHTML = list;
+    
+}
+
+mostrar("Grass");
+
+/*
+let list  = '';
+filtrarTipo('Electric').forEach((pokemones)  =>  {
+  const li  = `<li>${pokemones.name}</li>`  + `<img src="${pokemones.img}">`;
+  list  = list  + li;
+})
+containerlist.innerHTML = list;
+*/
+
