148b0aa4d6c7c96865b97bfad4cce0de1f565dd9
zuriros <hanami33rosas@gmail.com>
2018-12-14 14:55:44 -0500

    experimentando con el html primera fase para poder aplicar los datos y despues poder insertar los eventos

--- src/data.js
+++ src/data.js
@@ -1,8 +1,15 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+
+
+
+
+document.getElementById("demo").innerHTML = POKEMON.pokemon[0].weaknesses;
+
+
