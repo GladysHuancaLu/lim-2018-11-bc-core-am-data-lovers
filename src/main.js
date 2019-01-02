2cd4dab189a8c5e11354364a757ba7640f61bd6c
GladysHuancaLu <gladys.luana123@gmail.com>
2019-01-02 13:42:00 -0500

    Funcion filterEvolution y correcion funcion sorData

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ secondWindow.style.display = 'none';
 
 firstButton.addEventListener('click', function() {
   firstWindow.style.display = 'none';
-  secondWindow.style.display = 'block';
-  // thirdWindow.style.display = "none";
+  containerlist.style.display = 'none';
+  containerCandy.style.display = 'none';
+  containerOrdenar.style.display = 'none';
+  secondWindow.style.display = 'inline-flex';
 });
 
 const dataPokemon = POKEMON.pokemon;
@@ -18,13 +20,6 @@ const listaTipos = document.getElementById('typesPokemon');
 for (let i = 0; i < arrayTiposUnicos.length; i++) {
   listaTipos.innerHTML += `<option value= ${arrayTiposUnicos[i]}>${arrayTiposUnicos[i]}</option>`
 }
-/*
-let textArray = [];
-for (let i = 0; i < dataPokemon.length; i++) {
-  textArray = textArray.concat(dataPokemon[i].weaknesses);
-}
-console.log(textArray);
-*/
 
 const containerlist = document.getElementById('contFiltrar');
 listaTipos.addEventListener('change', mostrar);
@@ -33,37 +28,40 @@ function mostrar() {
   const data = pokemon.filtrarTipo(listaTipos.value, dataPokemon);
   containerOrdenar.style.display = 'none';
   containerCandy.style.display = 'none';
-  containerlist.style.display = 'block';
+  containerlist.style.display = 'inline-flex';
   containerlist.innerHTML = pokemon.mostrarTemplates(data);
-};
+}
 
 const containerOrdenar = document.getElementById('contOrdenar');
 const listaOrdenar = document.getElementById('orderingPokemon');
 listaOrdenar.addEventListener('change', mostrarOrden);
 function mostrarOrden() {
-  const data = pokemon.sortData(dataPokemon, 'name', listaOrdenar.value);
+  const data = pokemon.sortData(dataPokemon, listaOrdenar.value);
   containerlist.style.display = 'none';
   containerCandy.style.display = 'none';
-  containerOrdenar.style.display = 'block';
+  containerOrdenar.style.display = 'inline-flex';
   containerOrdenar.innerHTML = pokemon.mostrarTemplates(data);
-};
+}
 
 const containerCandy = document.getElementById('contCandy');
+containerCandy.style.display = 'none';
 const btnCandy = document.getElementById('candy');
 btnCandy.addEventListener('click', () => {
   containerOrdenar.style.display = 'none';
   containerlist.style.display = 'none';
-  containerCandy.style.display = 'block';
+  containerCandy.style.display = 'inline-flex';
 });
 
 const btnBuscar = document.getElementById('btnBuscar');
 const inputBuscar = document.getElementById('nombre');
 btnBuscar.addEventListener('click', () => {
   const data = pokemon.filterEvolution(dataPokemon, inputBuscar.value);
+  console.log(data);
+  
   let list = '';
-  for (let i = 0; i < data.length-1; i++) {
+  for (let i = 0; i < data.length - 1; i++) {
     const card = `
-     <div class ='card-link'>
+     <div class ='card-link2 pseudoelemento'>
       <article class ='blog-card'>
         <img class="post-image" src="${ data[i].img }" />
         <div class="article-details">
@@ -81,7 +79,7 @@ btnBuscar.addEventListener('click', () => {
   }
   const ultimaEvolucion = data[data.length - 1 ];
   const card2 = `
-   <div class ='card-link'>
+   <div class ='card-link2'>
     <article class ='blog-card'>
       <img class="post-image" src="${ ultimaEvolucion.img }" />
       <div class="article-details">
@@ -98,59 +96,3 @@ btnBuscar.addEventListener('click', () => {
   list = list + card2;
   containerCandy.innerHTML = list;
 });
- 
-  /*
-  data.forEach((pokemones) => {
-    if (data.hasOwnProperty('next_evolution')) {
-      const card = `
-     <div class ='card-link'>
-      <article class ='blog-card'>
-        <img class="post-image" src="${ pokemones.img }" />
-        <div class="article-details">
-          <h4 class="post-title">${ pokemones.name }</h4>
-          <h3 class="post-category">${ pokemones.type }</h3>
-        </div>
-        <div class="article-details">
-          <h3 class="post-category">Tipo de caramelo: ${ pokemones.candy }</h3>
-          <h4 class="post-title">Caramelos para evolucionar: ${pokemones.candy_count}</h4>
-        </div>
-      </article>
-      </div>
-     `;
-      list = list + card;
-    }
-    else {
-      const card = `
-     <div class ='card-link'>
-      <article class ='blog-card'>
-        <img class="post-image" src="${ pokemones.img }" />
-        <div class="article-details">
-          <h4 class="post-title">${ pokemones.name }</h4>
-          <h3 class="post-category">${ pokemones.type }</h3>
-        </div>
-        <div class="article-details">
-          <h3 class="post-category">Tipo de caramelo: ${ pokemones.candy }</h3>
-          <h4 class="post-title">${pokemones.name} Alcanzo la maxima evolucion</h4>
-        </div>
-      </article>
-     </div>
-     `;
-      list2 = list2 + card;
-    }
-  });
-
-  containerCandy.innerHTML = list + list2;  
-  //containerCandy.innerHTML = pokemon.mostrarTemplates(pokemon.filterEvolution(dataPokemon, inputBuscar.value));
-});
-*/
-
-/*
-const ordenarAscendente = () => {
-  const textoNombre = document.getElementById("nombre");
-  let arrOrdenado = arrLol.map( arrLol => arrLol.name).sort();
-  return textoNombre.innerHTML = arrOrdenado;
- };
-POKEMON.pokemon.name.sort(
-*/
-
-//event.target.getAttribute('value')
